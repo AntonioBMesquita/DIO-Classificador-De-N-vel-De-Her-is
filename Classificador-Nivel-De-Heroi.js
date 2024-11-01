84245198878bab638d90e40dc38ad032d22e8dd3
Antonio Mesquita <164540995+AntonioBMesquita@users.noreply.github.com>
2024-11-01 15:04:19 -0300

    Update Classificador-Nivel-De-Heroi.js

--- Classificador-Nivel-De-Heroi.js
+++ Classificador-Nivel-De-Heroi.js
@@ -1,28 +1,31 @@
-let heroi="Hasgard"
-let xp = 12345
-let ranking
-if(xp<=1000){
-ranking = "Ferro"
+let heroi = "Hasgard";
+let xp = 9000;
+let ranking;
+
+switch (true) {
+    case (xp <= 1000):
+        ranking = "Ferro";
+        break;
+    case (xp > 1000 && xp <= 2000):
+        ranking = "Bronze";
+        break;
+    case (xp > 2000 && xp <= 5000):
+        ranking = "Prata";
+        break;
+    case (xp > 5000 && xp <= 7000):
+        ranking = "Ouro";
+        break;
+    case (xp > 7000 && xp <= 8000):
+        ranking = "Platina";
+        break;
+    case (xp > 8000 && xp <= 9000):
+        ranking = "Ascendente";
+        break;
+    case (xp > 9000 && xp <= 10000): // Corrigido o valor de 1000 para 10000
+        ranking = "Imortal";
+        break;
+    default:
+        ranking = "Radiante";
 }
-else if(xp>1000 &&xp<=2000){
-ranking = "Bronze"
-}
-else if(xp >2000 && xp<=5000){
-ranking = "Prata"
-}
-else if (xp>5000 && xp<=7000){
-ranking = "Ouro"
-}
-else if (xp>7000 && xp<=8000){
-    ranking = "Platina"
-}
-else if (xp>8000 && xp<=9000){
-    ranking = "Ascendente"
-}
-else if (xp>9000 && xp<=1000){
-    ranking="Imortal"
-}
-else{
-    ranking="Radiante"
-}
-console.log(`O Herói de nome ${heroi} está no nível de ${ranking}`)
+
+console.log(`O Herói de nome ${heroi} está no nível de ${ranking}`);
